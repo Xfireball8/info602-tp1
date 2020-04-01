bcda80c77f3b4ad1af183743934d89e578c5a4bd
dilettaweasley <didi131313@gmail.com>
2020-04-01 17:32:09 +0200

    Put a Dialog to choose a image when clicking on Open button

--- main-window.c
+++ main-window.c
@@ -13,14 +13,50 @@ static GtkWidget *window = NULL;
 /* *
  * Fonction qui récupère le chemin d'accès d'une image sur l'ordinateur.
  * Et charge l'image correspondante dans le PixBuf.
- * */
+ 	 We choose the image from the computer.
+	 To do that we need to create a file chooser dialog.
+  */
 
 void
 open_file (	GtkToolButton	*toolbutton,
 		gpointer	user_data)
 {
-	/* Testing if the signal is working...*/
-	printf("Ok.");
+	/*
+		Variables
+	*/
+	GtkWidget *dialog; // Dialog is indeed a GtkWidget (Everything that is draw on the screen herit from GtkWidget)
+	GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN; // don't worry about this for now...
+	gint res; // This variable is for the result we obtain when we close the window (Either file chosen or closed)
+
+	dialog = gtk_file_chooser_dialog_new ("Choisir Image",
+                                      window,
+                                      action,
+                                      _("_Cancel"), // For cancel event
+                                      GTK_RESPONSE_CANCEL, // The window send back this result message
+                                      _("_Open"), // For Open event
+                                      GTK_RESPONSE_ACCEPT, // The window send back GTK_RESPONSE_ACCEPT message
+                                      NULL);
+
+res = gtk_dialog_run (GTK_DIALOG (dialog)); // We're running the dialog now
+
+if (res == GTK_RESPONSE_ACCEPT) // If the user indeed provided us with a file...
+  {
+    char *filename; // We create a string for the path of the file
+
+		/* We create a GtkFileChooser to get the path of the file.
+				(This object is responsible of getting information from the dialog)*/
+
+		GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
+
+		// We create the file chooser by giving him the dialog we create earlier
+		filename = gtk_file_chooser_get_filename (chooser);
+
+		// What we do there is simply opening the file
+		// TODO : Test if the file is indeed a .png file
+		//				and do not open it if it's the case.
+    open_file (filename);
+    g_free (filename);
+  }
 }
 
 /* MAIN : Lance l'application et retourne le status d'execution de
@@ -55,8 +91,17 @@ main (	int 	argc,
 
 	/* Connexion des signaux du builder */
 	// We tell the builder to connect the signals of the buttons
-	gtk_builder_connect_signals (	builder,
-					NULL);
+
+	// Makes GtkBuilder know that a function name open_file does exists
+	gtk_builder_add_callback_symbol (	builder,
+																		"open_file",
+																		G_CALLBACK(open_file));
+
+
+	/* Works for MacOs Catalina but not on Ubuntu 19.10 */
+		gtk_builder_connect_signals (	builder,
+						NULL);
+
 
 	/* Fermer la fenetre lorsque l'on appuie sur la croix rouge */
 	// In this particular case, i do it manually
