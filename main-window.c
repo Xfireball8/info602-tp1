5fb39fe649f2232b950e50b4f85869fc65d3ff82
faisalsalhi <faisal.salhi@etu.univ-smb.fr>
2020-04-02 13:01:33 +0200

    Implementing Input and Output button and effects, implementing the scale for the grey level

--- main-window.c
+++ main-window.c
@@ -12,6 +12,64 @@
 static GtkWidget *window = NULL;
 static GtkBuilder *builder = NULL; // UI Builder pour construire l'interface à partir du fichier XML UI_FILE
 
+/**
+    Un pixel est une structure de 3 octets (rouge, vert, bleu). On les
+    plaque au bon endroit dans un pixbuf pour modifier les couleurs du pixel.
+  */
+typedef struct {
+   guchar rouge;
+   guchar vert;
+   guchar bleu;
+} Pixel;
+
+/**
+    Retourne le niveau de gris du pixel.
+ */
+unsigned char greyLevel( Pixel* data )
+{
+   return (data->rouge+data->vert+data->bleu)/3;
+}
+
+/**
+    Met le pixel au niveau de gris \a g.
+ */
+void setGreyLevel( Pixel* data, unsigned char g )
+{
+  data->rouge = g;
+  data->vert  = g;
+  data->bleu  = g;
+}
+
+/* Fonction qui permet d'afficher le buffer d'output */
+void
+make_output_buffer_visible()
+{
+	GObject *image_output_buffer;
+	GObject *image_input_buffer;
+
+	image_input_buffer = gtk_builder_get_object (builder, "input-image-buffer");
+	image_output_buffer = gtk_builder_get_object (builder, "output-image-buffer");
+
+	gtk_widget_hide(GTK_WIDGET(image_input_buffer));
+	gtk_widget_show(GTK_WIDGET(image_output_buffer));
+}
+
+void
+make_input_buffer_visible()
+{
+	GObject *image_output_buffer;
+	GObject *image_input_buffer;
+
+	image_input_buffer = gtk_builder_get_object (builder, "input-image-buffer");
+	image_output_buffer = gtk_builder_get_object (builder, "output-image-buffer");
+
+	gtk_widget_show(GTK_WIDGET(image_input_buffer));
+	gtk_widget_hide(GTK_WIDGET(image_output_buffer));
+}
+
+
+/* Fonction qui met à jour le pixbuf input avec une image
+	chargé depuis le système. */
 
 void
 update_image_buffer(char *filename)
@@ -20,7 +78,7 @@ update_image_buffer(char *filename)
 	GObject *image;
 	GdkPixbuf *pixbuf;
 
-	image = gtk_builder_get_object (builder, "image");
+	image = gtk_builder_get_object (builder, "input-image-buffer");
 	pixbuf = gdk_pixbuf_new_from_file_at_size (	filename,
 																							IMAGE_BUFFER_WIDTH,
 																							IMAGE_BUFFER_HEIGHT,
@@ -121,8 +179,13 @@ main (	int 	argc,
 																		"open_file",
 																		G_CALLBACK(open_file));
 
+	gtk_builder_add_callback_symbol (	builder,
+																	  "make_output_buffer_visible",
+																		G_CALLBACK(make_output_buffer_visible));
+	gtk_builder_add_callback_symbol (	builder,
+																		"make_input_buffer_visible",
+																		G_CALLBACK(make_input_buffer_visible));
 
-	/* Works for MacOs Catalina but not on Ubuntu 19.10 */
 		gtk_builder_connect_signals (	builder,
 						NULL);
 
